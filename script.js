e490f7e6c70db5ca12fe4135be1d05d3decc910d
ResTra23 <bsurianto28@gmail.com>
2025-05-09 21:31:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let score = 0
+
+const tolong = new Audio("assets/sounds/dittolongin.mp3")
+const sfx = new Audio("assets/sounds/on-or-off-light-switch-tap.mp3")
+const scoreDisplay = document.getElementById("jumlah")
+const dennis = document.getElementById("target")
+
+dennis.addEventListener("click", function() {
+    score += 1
+    scoreDisplay.textContent = score
+
+    sfx.currentTime = 0
+    sfx.play()
+})
+
+let mulai = false
+
+dennis.addEventListener("click", function() {
+    if (!mulai) {
+        mulai = true
+        setInterval(() => {
+           tolong.currentTime = 0
+           tolong.play() 
+        }, 15000);
+    }
+})
